c62f4ba8c4420c00c9f7c1dd95cb59c16cbd78a5
Miguel <miguel@steelswantechnologies.com>
2025-04-06 13:50:39 -0700

    Ch2, lvl4

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,3 +17,41 @@ class HtmlNode:
         value_str = f"value={self.value}" if self.value else ""
 
         return f"HtmlNode(tag='{self.tag}'{value_str}{children_str}, props={{{props_str}}})"
+
+class HtmlNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+    
+    def props_to_html(self):
+        return " ".join([f"{key}=\"{value}\"" for key, value in self.props.items()])
+    
+    def __repr__(self):
+        props_str = self.props_to_html() if self.props else ""
+        children_str = f"children={self.children}" if self.children else ""
+        value_str = f"value={self.value}" if self.value else ""
+
+        return f"HtmlNode(tag='{self.tag}'{value_str}{children_str}, props={{{props_str}}})"
+
+class LeafNode(HtmlNode):
+    def __init__(self, tag, value, props=None):
+        # Call the parent class's __init__ method
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+    
+        if self.tag == None:
+            return self.value
+        
+        props_html = ""
+        if self.props:
+            props_html = " " + self.props_to_html()
+            
+        return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
